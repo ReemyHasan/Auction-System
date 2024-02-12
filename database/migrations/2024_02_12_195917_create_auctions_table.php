f8e0d8da2990831ac25f36122b749e4e9bb98783
ReemyHasan <reemhasan2000831@gmail.com>
2024-02-12 23:08:42 +0300

    add tables to DB

---
+++ database/migrations/2024_02_12_195917_create_auctions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('auctions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained('products')->cascadeOnDelete();
+            $table->double('lowest_price')->default(0);
+            $table->double('closing_price')->default(0);
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->time('start_time');
+            $table->time('closing_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('auctions');
+    }
+};
