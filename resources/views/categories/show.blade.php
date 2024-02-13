4ac3ce28c982781b266ee5d272ad6bb3523a2d60
ReemyHasan <reemhasan2000831@gmail.com>
2024-02-13 17:31:49 +0300

    show category

---
+++ resources/views/categories/show.blade.php
@@ -0,0 +1,39 @@
+@extends('layout.app')
+@section('content')
+    <div class="content-wrapper">
+        <section class="content-header pb-0" >
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-12">
+                        <h3>{{$category->name}}</h3>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
+        <section class="content pt-0">
+            <div class="container-fluid">
+                <div class="row">
+                    <!-- left column -->
+                    <div class="col-md-12">
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        <label for="name">name:</label>
+                                        <h5>{{$category->name}}</h5>
+                                    </div>
+                                    <hr>
+                                    <div class="form-group">
+                                        <label for="description">description:</label>
+                                        <h5>{{$category->description}}</h5>
+
+                                    </div>
+                                </div>
+                        </div>
+                </div>
+
+            </div>
+    </div>
+    </section>
+    </div>
+@endsection
