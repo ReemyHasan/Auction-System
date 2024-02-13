51e592d17c52769f9b47e748622f5bbaa75aed28
ReemyHasan <reemhasan2000831@gmail.com>
2024-02-13 18:30:11 +0300

    active nav-link

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -4,14 +4,14 @@
         <h1 class="logo"><a href="{{url('/')}}">Auction System</a></h1>
         <nav id="navbar" class="navbar">
             <ul>
-                <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+                <li><a class="nav-link scrollto {{ Route::is('home') ? 'active' : '' }}" href="{{route('home')}}">Home</a></li>
                 <li><a class="nav-link scrollto" href="#about">About</a></li>
                 @if (!Auth::check())
-                    <li><a class="nav-link scrollto" href="{{ route('login') }}">login</a></li>
-                    <li><a class="nav-link scrollto o" href="{{ route('register') }}">Register</a></li>
+                    <li><a class="nav-link scrollto " href="{{ route('login') }}">login</a></li>
+                    <li><a class="nav-link scrollto " href="{{ route('register') }}">Register</a></li>
                 @else
                     <li><a class="nav-link scrollto" href="{{ route('logout') }}">logout</a></li>
-                    <li><a class="nav-link scrollto" href="{{ route('categories.index') }}">categories</a></li>
+                    <li><a class="nav-link scrollto {{ Route::is('categories.index') ? 'active' : '' }}" href="{{ route('categories.index') }}">categories</a></li>
                 @endif
 
             </ul>
