8dc9c0cb846a847e88f7f5d8e02801d51a4b8f01
ReemyHasan <reemhasan2000831@gmail.com>
2024-02-17 21:06:39 +0300

    Hash password

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -2,9 +2,11 @@
 
 namespace App\Services;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 class UserService
 {
     public function create($data){
+        $data['password']= Hash::make($data['password']);
         return User::create($data);
     }
 
