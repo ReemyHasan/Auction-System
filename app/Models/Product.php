0a98f529fe25778f190b183bade8d187bc8cb6f3
ReemyHasan <reemhasan2000831@gmail.com>
2024-02-20 12:26:32 +0300

    fix filtring in products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -22,11 +22,13 @@ class Product extends BaseModel
     protected $with = [
         "user"
     ];
-    public function categories(){
-        return $this->belongsToMany(Category::class,'product_category','product_id','category_id')->withTimestamps();
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'product_category', 'product_id', 'category_id')->withTimestamps();
     }
-    public function user(){
-        return $this->belongsTo(User::class,"vendor_id");
+    public function user()
+    {
+        return $this->belongsTo(User::class, "vendor_id");
     }
     public function get_imageUrl()
     {
@@ -34,18 +36,21 @@ class Product extends BaseModel
             return url('storage/products/' . $this->image);
         }
     }
-    public function scopeFilter($query){
-        if(request()->has("name")){
-            $query->where("name","like","%".request()->get("name")."%");
+    public function scopeFilter($query)
+    {
+        if (request()->has("name")) {
+            $query->where("name", "like", "%" . request()->get("name") . "%");
         }
-        if(request()->has("created_at")){
-            $query->where("created_at","like","%".request()->get("created_at")."%");
+        if (request()->has("created_at")) {
+            $query->where("products.created_at", "like", "%" . request()->get("created_at") . "%");
         }
-        // if(request()->has("category_id")){
-        //     $query->where("category_id","like","%".request()->get("category_id")."%");
-        // }
-        if(request()->has("my_products")){
-            $query->where('vendor_id','=',Auth::user()->id);
+        if (request()->has("category_id")) {
+            $query->join("product_category", "products.id", "=", "product_category.product_id")
+                ->where("category_id", "like", "%" . request()->get("category_id") . "%")
+                ->select('products.*');
+        }
+        if (request()->has("my_products")) {
+            $query->where('vendor_id', '=', Auth::user()->id);
         }
     }
     public function interactions(): MorphMany
