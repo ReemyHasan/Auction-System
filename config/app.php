86efc610688fb90a45d21e80639df16ba524a580
ReemyHasan <reemhasan2000831@gmail.com>
2024-02-26 00:01:54 +0300

    experiment

--- config/app.php
+++ config/app.php
@@ -168,7 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        NotificationChannels\Telegram\TelegramServiceProvider::class
+        // NotificationChannels\Telegram\TelegramServiceProvider::class
     ])->toArray(),
 
     /*
